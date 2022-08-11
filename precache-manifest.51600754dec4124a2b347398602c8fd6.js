9fc55547755a36fc1533f0d1d8c45357f50e611b
dev28k <faizkhan.dev@gmail.com>
2022-08-12 00:06:00 +0500

    Updates

---
+++ precache-manifest.51600754dec4124a2b347398602c8fd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dd05bb1c43b2cbbc53d",
+    "url": "/tozo-sandbox/static/css/main.bdf0f608.chunk.css"
+  },
+  {
+    "revision": "1dd05bb1c43b2cbbc53d",
+    "url": "/tozo-sandbox/static/js/main.1dd05bb1.chunk.js"
+  },
+  {
+    "revision": "083a92f3bb60d465e079",
+    "url": "/tozo-sandbox/static/js/1.083a92f3.chunk.js"
+  },
+  {
+    "revision": "176e38a65a6fa6a7d077",
+    "url": "/tozo-sandbox/static/css/2.77956353.chunk.css"
+  },
+  {
+    "revision": "176e38a65a6fa6a7d077",
+    "url": "/tozo-sandbox/static/js/2.176e38a6.chunk.js"
+  },
+  {
+    "revision": "c31858c08f1f712c8b6f",
+    "url": "/tozo-sandbox/static/js/runtime~main.c31858c0.js"
+  },
+  {
+    "revision": "f7ae8aea8ee7ff394fe902f34184b0b0",
+    "url": "/tozo-sandbox/static/media/logo.f7ae8aea.png"
+  },
+  {
+    "revision": "8a44ddaea9166515bea10b3ab7ff5fab",
+    "url": "/tozo-sandbox/static/media/navFooter.8a44ddae.png"
+  },
+  {
+    "revision": "de33308721e023dcd72191514e6eb1e4",
+    "url": "/tozo-sandbox/index.html"
+  }
+];
