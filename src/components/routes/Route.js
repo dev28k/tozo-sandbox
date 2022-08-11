1ea42a71938c0d0851184819157602ee3eaddeb1
dev28k <faizkhan.dev@gmail.com>
2022-08-12 00:14:54 +0500

    done

--- src/components/routes/Route.js
+++ src/components/routes/Route.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, HashRouter } from "react-router-dom";
 import Dashboard from "../Dashboard";
 import About from "../About/About";
 import PublishAll from "../PublishAll/PublishAll";
@@ -8,13 +8,13 @@ import RSSFeed from "../RSSfeed/RSS";
 
 export default function Routerr() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={<Dashboard />} />
         <Route exact path="/publishAll" element={<PublishAll />} />
         <Route exact path="/rss" element={<RSSFeed />} />
         <Route exact path="/about" element={<About />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
